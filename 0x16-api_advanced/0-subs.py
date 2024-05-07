79e8d47813f29c96a2017f51084c749b86af638d
OussamaZaima <oussama.zaima2002@gmail.com>
2024-05-07 16:15:17 -0700

    alltasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A module that returns the number of subscribers for a given subreddit """
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = get(url, headers=user_agent, allow_redirects=False)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
